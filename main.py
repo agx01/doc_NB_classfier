9cae543949841ac221bf91aadcb0c2bd48f2b082
Arijit Ganguly <arijit.ganguly004@gmail.com>
2021-10-08 20:55:17 -0500

    Get the list of sub-driectories as the classes.

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ Created on Fri Oct  8 19:38:45 2021
 
 @author: Arijit Ganguly
 """
+import os
+
+class DocClassifier:
+    
+    def __init__(self):
+        self.labels = [x for x in os.listdir("data/20_newsgroups")]
+        print(self.labels)
+
+if __name__ == "__main__":
+    doc_class = DocClassifier()
--- test.py
+++ test.py
@@ -4,3 +4,10 @@ Created on Fri Oct  8 19:38:59 2021
 
 @author: Arijit Ganguly
 """
+
+import os
+
+labels = [x for x in os.listdir("data/20_newsgroups")]
+
+print(labels)
+
