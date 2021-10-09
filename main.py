f8480a8fa79d2876e97f91d13dc1610df56ae25a
Arijit Ganguly <arijit.ganguly004@gmail.com>
2021-10-09 03:06:02 -0500

    Reading and printing the document works.Added to main file

--- main.py
+++ main.py
@@ -10,7 +10,25 @@ class DocClassifier:
     
     def __init__(self):
         self.labels = [x for x in os.listdir("data/20_newsgroups")]
-        print(self.labels)
-
+        self.path = "data\\20_newsgroups\\"
+    
+    def read_file(self, file_path):
+        with open(file_path, 'r') as f:
+            #print(f.read())
+            print(file_path)
+    
+    def print_filenames(self):
+        #iterate through the sub directories
+        for label in self.labels:
+            k = 0
+            #Iterate through the files in sub directories
+            for file in os.listdir(f"{self.path}\{label}"):
+                file_path = f"{self.path}\{label}\{file}"
+                self.read_file(file_path)
+                k += 1
+        
+        print("Number of Files"+str(k))
+        
 if __name__ == "__main__":
     doc_class = DocClassifier()
+    doc_class.print_filenames()
--- test.py
+++ test.py
@@ -7,7 +7,24 @@ Created on Fri Oct  8 19:38:59 2021
 
 import os
 
+# Get labels/classes of the documents
 labels = [x for x in os.listdir("data/20_newsgroups")]
 
 print(labels)
 
+path = "data\\20_newsgroups\\"
+def read_file(file_path):
+    with open(file_path, 'r') as f:
+        #print(f.read())
+        print(file_path)
+
+#iterate through the sub directories
+for label in labels:
+    k = 0
+    #Iterate through the files in sub directories
+    for file in os.listdir(f"{path}\{label}"):
+        file_path = f"{path}\{label}\{file}"
+        read_file(file_path)
+        k += 1
+
+print("Number of Files"+str(k))
