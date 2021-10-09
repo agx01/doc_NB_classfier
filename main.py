89cab907feefa5b8ea92b2f05a1e5366f1e18562
Arijit Ganguly <arijit.ganguly004@gmail.com>
2021-10-08 20:21:39 -0500

    Initial commit. Folder Structure setup and data downloaded for the required task

---
+++ main.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  8 19:38:45 2021
+
+@author: Arijit Ganguly
+"""
---
+++ test.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  8 19:38:59 2021
+
+@author: Arijit Ganguly
+"""
