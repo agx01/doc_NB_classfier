1048f48abf1fe26b036d791bae32ef7d59f4fdf3
Arijit Ganguly <arijit.ganguly004@gmail.com>
2021-10-10 16:21:32 -0500

    Preprocessing of documents work, Need to check the preprocessed content again to make sure Count2Vec work

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ Use the data provided by the website to classify 1000 documents into 20 newsgrou
 
 **Data :**
 https://www.cs.cmu.edu/afs/cs/project/theo-11/www/naive-bayes.html
+
+## Stop words setup required
+The code utilizes a **nltk** library for importing of the stopwords.
+To setup, please use the below mentioned code in the Python console.
+```
+import nltk
+nltk.download('stopwords')
+```
--- main.py
+++ main.py
@@ -5,6 +5,10 @@ Created on Fri Oct  8 19:38:45 2021
 @author: Arijit Ganguly
 """
 import os
+import re
+import string
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
 
 class DocClassifier:
     
@@ -14,21 +18,53 @@ class DocClassifier:
     
     def read_file(self, file_path):
         with open(file_path, 'r') as f:
-            #print(f.read())
-            print(file_path)
-    
+            content = f.read()
+        return content
+        
     def print_filenames(self):
+        k=0
         #iterate through the sub directories
         for label in self.labels:
-            k = 0
             #Iterate through the files in sub directories
             for file in os.listdir(f"{self.path}\{label}"):
                 file_path = f"{self.path}\{label}\{file}"
                 self.read_file(file_path)
                 k += 1
-        
         print("Number of Files"+str(k))
+    
+    def remove_stop_words(self, stop_words, content):
+        return " ".join([word for word in str(content).split() if word not in stop_words])
+    
+    def prepare_data(self):
+        #Initializing the stopwords
+        stop_words = set(stopwords.words('english'))
+        
+        k =0
+        #iterate through the sub directories
+        for label in self.labels:
+            #Iterate through the files in sub directories
+            for file in os.listdir(f"{self.path}\{label}"):
+                file_path = f"{self.path}\{label}\{file}"
+                content = self.read_file(file_path)
+                
+                #Lower case the entire content
+                content = content.lower()
+                
+                #Remove the punctuation
+                content = re.sub('[%s]' % re.escape(string.punctuation), '', content)
+                
+                #Remove words and digits like game47,g4me
+                content = re.sub('W*dw*', '', content)
+                
+                #Remove stopwords
+                content = self.remove_stop_words(stop_words, content)                  
+                k += 1
+                print("Document %d preprocessing complete", k)    
+                
+        
+        print("Documents prepared: "+str(k))
         
 if __name__ == "__main__":
     doc_class = DocClassifier()
-    doc_class.print_filenames()
+    #doc_class.print_filenames()
+    doc_class.prepare_data()
