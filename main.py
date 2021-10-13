ff69176f362b108a164313fbedb865857a487187
Arijit Ganguly <arijit.ganguly004@gmail.com>
2021-10-13 01:52:06 -0500

    Docstrings updated and file count for each file displayed

--- main.py
+++ main.py
@@ -17,6 +17,21 @@ class DocClassifier:
         self.path = "data\\20_newsgroups\\"
     
     def read_file(self, file_path):
+        """
+        This function is to read the file specified in the file path 
+        and return the content of the file.
+
+        Parameters
+        ----------
+        file_path : String
+            The file path suffixed by the file name that needs to be read
+
+        Returns
+        -------
+        content : String
+            raw content of the file in string format
+
+        """
         with open(file_path, 'r') as f:
             content = f.read()
         return content
@@ -33,9 +48,50 @@ class DocClassifier:
         print("Number of Files"+str(k))
     
     def remove_stop_words(self, stop_words, content):
+        """
+        Function to remove the stop words from the content.
+        For example: the,a, an
+        
+        Process followed by the code:
+            1. Split the content into words
+            2. Loop through each word of the content and check if it exists
+                it exists in the list of stop words
+            3. Add into a new list if the word does not exist in the stop
+                words list
+            4. join the list of words to form a new string and return it
+
+        Parameters
+        ----------
+        stop_words : List[String]
+            List of stop words that need to be removed from content of file    
+        
+        content : String
+            The content that needs to be filtered for stop words
+
+        Returns
+        -------
+        String
+            Content that has been filtered out for any stop words
+
+        """
         return " ".join([word for word in str(content).split() if word not in stop_words])
     
     def prepare_data(self):
+        """
+        Function to pre-process the data mentioned in the files for each 
+        class/label to be able to convert the content into frequency vectors
+        
+        Pre-Process Steps for each file:
+            1. Lower any capital letters
+            2. Replace all punctuations with a space
+            3. Remove all words with letter/number combo
+            4. Remove all the stop words in the content
+
+        Returns
+        -------
+        None.
+
+        """
         #Initializing the stopwords
         stop_words = set(stopwords.words('english'))
         
@@ -50,8 +106,8 @@ class DocClassifier:
                 #Lower case the entire content
                 content = content.lower()
                 
-                #Remove the punctuation
-                content = re.sub('[%s]' % re.escape(string.punctuation), '', content)
+                #Replace the punctuation
+                content = re.sub('[%s]' % re.escape(string.punctuation), ' ', content)
                 
                 #Remove words and digits like game47,g4me
                 content = re.sub('W*dw*', '', content)
@@ -59,12 +115,18 @@ class DocClassifier:
                 #Remove stopwords
                 content = self.remove_stop_words(stop_words, content)                  
                 k += 1
-                print("Document %d preprocessing complete", k)    
-                
-        
+                print(f"Document {k} preprocessing complete")    
         print("Documents prepared: "+str(k))
+        
+    def count_files(self):
+        file_per_class = {}
+        for label in self.labels:
+            file_list = os.listdir(f"{self.path}\{label}")
+            file_per_class[label] = len(file_list)
+        print(file_per_class)
         
 if __name__ == "__main__":
     doc_class = DocClassifier()
     #doc_class.print_filenames()
-    doc_class.prepare_data()
+    #doc_class.prepare_data()
+    doc_class.count_files()
